7a02b428110599d3d0b838f5a01322f482abd423
Chris-OR <32755139+Chris-OR@users.noreply.github.com>
2022-02-17 15:54:17 -0700

    init flask

--- main.py
+++ main.py
@@ -37,10 +37,10 @@ URL_LIST = ["https://www.amazon.com/BLACK-DECKER-Dustbuster-Cordless-CHV1410L/dp
 #     'almBrandId': 'undefined'
 # }
 #
-# app = Flask(__name__)
-# app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
-#
-#
+app = Flask(__name__)
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
+
+
 def get_webpage(url, headers, cookies):
     print(f"Trying to load Amazon page...")
     searching = True
@@ -183,41 +183,42 @@ def send_change_location_request(zip_code: str, headers: dict, cookies: dict):
 
 
 def get_session_cookies(zip_code: str):
-    for URL in URL_LIST :
-        response = requests.get(url=AMAZON_US_URL, headers=DEFAULT_REQUEST_HEADERS)
-        content = Selector(text=response.text)
+    while True:
+        for URL in URL_LIST :
+            response = requests.get(url=AMAZON_US_URL, headers=DEFAULT_REQUEST_HEADERS)
+            content = Selector(text=response.text)
 
-        headers = {
-            "anti-csrftoken-a2z": get_ajax_token(content=content),
-            "user-agent": DEFAULT_USER_AGENT,
-        }
-        response = requests.get(
-            url=AMAZON_CSRF_TOKEN_URL, headers=headers, cookies=response.cookies
-        )
-        content = Selector(text=response.text)
+            headers = {
+                "anti-csrftoken-a2z": get_ajax_token(content=content),
+                "user-agent": DEFAULT_USER_AGENT,
+            }
+            response = requests.get(
+                url=AMAZON_CSRF_TOKEN_URL, headers=headers, cookies=response.cookies
+            )
+            content = Selector(text=response.text)
 
-        headers = {
-            "anti-csrftoken-a2z": get_token(content=content),
-            "user-agent": DEFAULT_USER_AGENT,
-        }
-        send_change_location_request(
-            zip_code=zip_code, headers=headers, cookies=dict(response.cookies)
-        )
-        # Verify that location changed correctly.
-        response = requests.get(
-            url=URL, headers=DEFAULT_REQUEST_HEADERS, cookies=response.cookies
-        )
-        content = Selector(text=response.text)
-        webpage = response.text
+            headers = {
+                "anti-csrftoken-a2z": get_token(content=content),
+                "user-agent": DEFAULT_USER_AGENT,
+            }
+            send_change_location_request(
+                zip_code=zip_code, headers=headers, cookies=dict(response.cookies)
+            )
+            # Verify that location changed correctly.
+            response = requests.get(
+                url=URL, headers=DEFAULT_REQUEST_HEADERS, cookies=response.cookies
+            )
+            content = Selector(text=response.text)
+            webpage = response.text
 
-        webpage_soup = BeautifulSoup(webpage, "html.parser")
-        handle_webpage(webpage_soup)
+            webpage_soup = BeautifulSoup(webpage, "html.parser")
+            handle_webpage(webpage_soup)
 
-        # location_label = content.css("span#glow-ingress-line2::text").get().strip()
-        # assert zip_code in location_label
+            # location_label = content.css("span#glow-ingress-line2::text").get().strip()
+            # assert zip_code in location_label
 
-        # get_webpage("https://www.amazon.com/DYSON-208992-01-Dyson-Total-Clean/dp/B011MACQ4O/ref=sr_1_18?m=A2L77EE7U53NWQ&pf_rd_i=10158976011&pf_rd_m=ATVPDKIKX0DER&pf_rd_p=27825c4b-ab2a-4439-a18e-8a6136972ae0&pf_rd_r=HE81QTTRH8KVW7EX4920&pf_rd_s=merchandised-search-5&pf_rd_t=101&qid=1644958427&rnid=10158976011&s=warehouse-deals&sr=1-18", headers, response.cookies)
-        time.sleep(50)
+            # get_webpage("https://www.amazon.com/DYSON-208992-01-Dyson-Total-Clean/dp/B011MACQ4O/ref=sr_1_18?m=A2L77EE7U53NWQ&pf_rd_i=10158976011&pf_rd_m=ATVPDKIKX0DER&pf_rd_p=27825c4b-ab2a-4439-a18e-8a6136972ae0&pf_rd_r=HE81QTTRH8KVW7EX4920&pf_rd_s=merchandised-search-5&pf_rd_t=101&qid=1644958427&rnid=10158976011&s=warehouse-deals&sr=1-18", headers, response.cookies)
+            time.sleep(50)
 
 
 if __name__ == "__main__":
