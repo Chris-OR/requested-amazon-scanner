b2519f27a1cd6bc57cd6ff36a8aed9cb7df0f0ff
Chris-OR <32755139+Chris-OR@users.noreply.github.com>
2022-02-17 16:05:44 -0700

    stop search

--- main.py
+++ main.py
@@ -225,7 +225,7 @@ def start_app():
     get_session_cookies(zip_code="62999")
 
 
-threading.Thread(target=start_app, daemon=True).start()
+# threading.Thread(target=start_app, daemon=True).start()
 
 
 if __name__ == "__main__":
