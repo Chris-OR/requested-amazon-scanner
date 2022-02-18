5a51dff0c2538d946d72ee939cb73e7fb0b00d97
Chris-OR <32755139+Chris-OR@users.noreply.github.com>
2022-02-18 14:16:56 -0700

    catch exception with invalid page content

--- main.py
+++ main.py
@@ -193,40 +193,44 @@ def send_change_location_request(zip_code: str, headers: dict, cookies: dict):
 def get_session_cookies(zip_code: str):
     while True:
         for URL in URL_LIST:
-            response = requests.get(url=AMAZON_US_URL, headers=DEFAULT_REQUEST_HEADERS)
-            content = Selector(text=response.text)
+            try:
+                response = requests.get(url=AMAZON_US_URL, headers=DEFAULT_REQUEST_HEADERS)
+                content = Selector(text=response.text)
 
-            headers = {
-                "anti-csrftoken-a2z": get_ajax_token(content=content),
-                "user-agent": DEFAULT_USER_AGENT,
-            }
-            response = requests.get(
-                url=AMAZON_CSRF_TOKEN_URL, headers=headers, cookies=response.cookies
-            )
-            content = Selector(text=response.text)
+                headers = {
+                    "anti-csrftoken-a2z": get_ajax_token(content=content),
+                    "user-agent": DEFAULT_USER_AGENT,
+                }
+                response = requests.get(
+                    url=AMAZON_CSRF_TOKEN_URL, headers=headers, cookies=response.cookies
+                )
+                content = Selector(text=response.text)
 
-            headers = {
-                "anti-csrftoken-a2z": get_token(content=content),
-                "user-agent": DEFAULT_USER_AGENT,
-            }
-            send_change_location_request(
-                zip_code=zip_code, headers=headers, cookies=dict(response.cookies)
-            )
-            # Verify that location changed correctly.
-            response = requests.get(
-                url=URL, headers=DEFAULT_REQUEST_HEADERS, cookies=response.cookies
-            )
-            content = Selector(text=response.text)
-            webpage = response.text
+                headers = {
+                    "anti-csrftoken-a2z": get_token(content=content),
+                    "user-agent": DEFAULT_USER_AGENT,
+                }
+                send_change_location_request(
+                    zip_code=zip_code, headers=headers, cookies=dict(response.cookies)
+                )
+                # Verify that location changed correctly.
+                response = requests.get(
+                    url=URL, headers=DEFAULT_REQUEST_HEADERS, cookies=response.cookies
+                )
+                content = Selector(text=response.text)
+                webpage = response.text
 
-            webpage_soup = BeautifulSoup(webpage, "html.parser")
-            handle_webpage(webpage_soup, URL)
+                webpage_soup = BeautifulSoup(webpage, "html.parser")
+                handle_webpage(webpage_soup, URL)
 
-            # location_label = content.css("span#glow-ingress-line2::text").get().strip()
-            # assert zip_code in location_label
+                # location_label = content.css("span#glow-ingress-line2::text").get().strip()
+                # assert zip_code in location_label
 
-            # get_webpage("https://www.amazon.com/DYSON-208992-01-Dyson-Total-Clean/dp/B011MACQ4O/ref=sr_1_18?m=A2L77EE7U53NWQ&pf_rd_i=10158976011&pf_rd_m=ATVPDKIKX0DER&pf_rd_p=27825c4b-ab2a-4439-a18e-8a6136972ae0&pf_rd_r=HE81QTTRH8KVW7EX4920&pf_rd_s=merchandised-search-5&pf_rd_t=101&qid=1644958427&rnid=10158976011&s=warehouse-deals&sr=1-18", headers, response.cookies)
-            time.sleep(75)
+                # get_webpage("https://www.amazon.com/DYSON-208992-01-Dyson-Total-Clean/dp/B011MACQ4O/ref=sr_1_18?m=A2L77EE7U53NWQ&pf_rd_i=10158976011&pf_rd_m=ATVPDKIKX0DER&pf_rd_p=27825c4b-ab2a-4439-a18e-8a6136972ae0&pf_rd_r=HE81QTTRH8KVW7EX4920&pf_rd_s=merchandised-search-5&pf_rd_t=101&qid=1644958427&rnid=10158976011&s=warehouse-deals&sr=1-18", headers, response.cookies)
+                time.sleep(75)
+            except ValueError:
+                print("ran into error when changing server location... trying again")
+                time.sleep(30)
 
 
 def send_telegram_message(title, price, URL):
